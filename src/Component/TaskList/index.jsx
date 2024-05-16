2079c41a303e0a07d6314b98bee4886b8e699363
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2024-05-16 23:31:46 +0700

    81

--- src/Component/TaskList/index.jsx
+++ src/Component/TaskList/index.jsx
@@ -1,13 +1,48 @@
-import { Skeleton, Pagination } from "antd";
+import { Skeleton, Pagination, Form, Input, Space } from "antd";
 import useFetching from "@/customHook/useFetching";
 import Render from "@/common/renderHelp";
-// import { element } from "prop-types";
-import { ReloadOutlined } from "@ant-design/icons";
+import { CloseOutlined, ReloadOutlined } from "@ant-design/icons";
 import { Button } from "antd";
-import { getTasks } from "../../services/task";
+import { createTask, getTasks } from "../../services/task";
+import { useRef, useState } from "react";
 
 function TaskList(Props) {
-  const { data, loading, error, loadPage, page,reload } = useFetching(getTasks);
+  const { data, loading, error, loadPage, page, reload } =
+    useFetching(getTasks);
+  const [form] = Form.useForm();
+  const [showAdd, setShowAdd] = useState(false);
+  const pendingApi = useRef(null);
+  const toggleAdd = () => {
+    setShowAdd(!showAdd);
+  };
+  const onFinish = async (value) => {
+    try {
+      let { title } = value;
+      form.resetFields();
+      pendingApi.current.disabled = true;
+      await createTask(title);
+      pendingApi.current.disabled = false;
+      reload();
+      setShowAdd(false);
+    } catch (error) {
+      pendingApi.current.disabled = false;
+      console.log(error);
+    }
+  };
+  const inputNew = (
+    <Form onFinish={onFinish} form={form}>
+      <Space>
+        <Form.Item name="title" style={{ marginBottom: 0 }}>
+          <Input  placeholder='Enter Task Title'></Input>
+        </Form.Item>
+        <Button ref={pendingApi} type="primary" htmlType="submit" >
+          Add
+        </Button>
+        <CloseOutlined onClick={toggleAdd}></CloseOutlined>
+      </Space>
+    </Form>
+  );
+
 
   const element = (
     <div className="list">
@@ -25,11 +60,29 @@ function TaskList(Props) {
           ? Array(10)
               .fill(0)
               .map((item, index) => <Skeleton key={index} active></Skeleton>)
-          : data?.map((item) => {
-              return <li key={item.id}>{item?.attributes?.title}</li>;
-            })}
+          : data
+              ?.sort((task1, task2) => {
+                let date1 = new Date(task1.attributes.createdAt);
+                let date2 = new Date(task2.attributes.createdAt);
+                if (date1 < date2) {
+                  return -1;
+                }
+                if (date1 > date2) {
+                  return 1;
+                }
+                if (date1 = date2) {
+                  return 0;
+                }
+              })
+              .map((item) => {
+                return <li key={item.id}>{item?.attributes?.title}</li>;
+              })}
+      
       </ul>
-      <button className="add-card-btn btn">Add a card</button>
+      {showAdd ? inputNew : null}
+      <button className="add-card-btn btn" onClick={toggleAdd}>
+        Add a card
+      </button>
     </div>
   );
 
--- src/services/task.jsx
+++ src/services/task.jsx
@@ -4,3 +4,13 @@ export const getTasks = async (page,pageSize,signal) =>{
    const response = await axios.get(`/tasks?pagination[page]=${page}&pagination[pageSize]=${pageSize}`, {signal});
    return response.data;
 }
+
+export const createTask = async (title)=>{
+   const response = await axios.post(`/tasks`,{
+         "data": {
+             "title": title
+         }
+     
+   });
+   return response.data;
+}
